198c4288c7618f24db6af96b322821f3fed779dc
StefanoPutelli <putellistefano@gmail.com>
2025-05-05 15:06:54 +0200

    you fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { MyAgent } from './MyAgent.js';
 
-const host = "https://deliveroojs25.azurewebsites.net/";
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImE5NmM2NCIsIm5hbWUiOiJhbm9ueW1vdXMiLCJyb2xlIjoidXNlciIsImlhdCI6MTc0NTg0MjM3NX0.Rc2L_gJGwdWXV9ZhXKwB8Vqxb5pY-kAuJ5XHLF7FdxA";
+const host = "http://localhost:8080/";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijg1ZDMwYSIsIm5hbWUiOiJjYWNhIiwidGVhbUlkIjoiZjliYmY5IiwidGVhbU5hbWUiOiJ0ZXN0MkB0ZXN0LmNvbSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQ2NDQ5MTI5fQ.dRhiQosDcuXaa3mk4bQzfR8S5i5PZMfwS7gwI0Dmf7Y";
 
 const pippo = new MyAgent(host , token);
 
