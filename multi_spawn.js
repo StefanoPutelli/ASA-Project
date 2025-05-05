f78fa5f3d14da2fdb6d8a0e20ff8c2ce7a01daae
robertodepaoli <roberto00.depaoli@gmail.com>
2025-05-05 16:33:55 +0200

    multi spawn

---
+++ multi_spawn.js
@@ -0,0 +1,26 @@
+// import child_process in ES module
+import { spawn } from 'child_process';
+
+// Function to spawn child processes
+function spawnProcesses() {
+    for ( let i = 0; i < 5; i++ ) {
+        const name = 'm_'+i;
+        
+        const childProcess = spawn( `node dist/index.js -name=${name}`, { shell: true } );
+
+        childProcess.stdout.on('data', data => {
+            console.log(`Output from ${name}: ${data}`);
+        });
+
+        childProcess.stderr.on('data', data => {
+            console.error(`Error from ${name}: ${data}`);
+        });
+
+        childProcess.on('close', code => {
+            console.log(`Child process ${name} exited with code ${code}`);
+        });
+    }
+}
+
+// Start the processes
+spawnProcesses();
