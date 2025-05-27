cf7d43565291eb3e4c6f21ece2f9d18af421ec3c
robertodepaoli <roberto00.depaoli@gmail.com>
2025-05-28 00:47:01 +0200

    alsdkja

--- src/multi_spawn.ts
+++ src/multi_spawn.ts
@@ -6,13 +6,15 @@ dotenv.config();
 
 // Genera una stringa casuale di 32 caratteri per la chiave segreta
 const secret_key = crypto.randomBytes(16).toString('hex'); // 32 caratteri esadecimali = 16 byte
-const host = process.env.SERVER_URL || "https://deliveroojs25.azurewebsites.net/";
+var host = process.env.SERVER_URL || "https://deliveroojs25.azurewebsites.net/";
 
-const p_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU3NTA2MyIsIm5hbWUiOiJwb2xwbyIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQ4MTkwODQ1fQ.Y1c8KqgPEfCaKK7mm-hsy4bUCrCleWtOhlQKdhb-IKQ";
-const s_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjdiYTQwZSIsIm5hbWUiOiJzZXBwaWEiLCJyb2xlIjoidXNlciIsImlhdCI6MTc0ODE5MDg2NH0.Z15_nxAzUqxv_Y3ujoTAe35So8V5gQ8iWoTt4ZClWUM";
+host = "http://192.168.23.242:8080/";
 
-const polpo = { name: "polpo", token: p_token}
-const seppia = { name: "seppia", token: s_token}
+const p_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQ2NGRhYyIsIm5hbWUiOiJjYW5lIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3NDgxOTEwNTR9.oQJOmKrJEkvoqNqn-CYyCzODVXxQtAonnjO7tuyEQRw";
+const s_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEwOWIyYyIsIm5hbWUiOiJnYXR0byIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQ4MTkxMDY2fQ.1Upx-K_ba4kFOzPF1N3vD0CStEx8DfWhvzXDTPlkCqE";
+
+const polpo = { name: "cane", token: p_token}
+const seppia = { name: "gatto", token: s_token}
 
 function spawnProcess(me : {
     name: string;
