c2669617e96ef626bc6b20e2ff2d1effcb9344b7
StefanoPutelli <putellistefano@gmail.com>
2025-06-09 15:50:26 +0200

    multi spawn fix

--- src/multi_spawn.ts
+++ src/multi_spawn.ts
@@ -8,13 +8,11 @@ dotenv.config();
 const secret_key = crypto.randomBytes(16).toString('hex'); // 32 caratteri esadecimali = 16 byte
 var host = process.env.SERVER_URL || "https://deliveroojs25.azurewebsites.net/";
 
-host = "http://192.168.23.242:8080/";
-
 const p_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImY2NWI1NiIsIm5hbWUiOiJwb2xwbyIsInRlYW1JZCI6ImU1MjViOSIsInRlYW1OYW1lIjoicmFpZGVycyIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQ4NDE4MzgzfQ.SuwUImEToE4ryXU4D-kAqVnvkZmrqQKcubow4BndZjU";
 const s_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImYzZTVkZSIsIm5hbWUiOiJzZXBwaWEiLCJ0ZWFtSWQiOiI5MWMwZmQiLCJ0ZWFtTmFtZSI6InJhaWRlcnMiLCJyb2xlIjoidXNlciIsImlhdCI6MTc0ODQxODM4OH0.0lImxPecY3gtGdFNASxbuJ4Z5u1OEAUJ5p0xGodxOQM";
 
-const polpo = { name: "polpo", token: p_token}
-const seppia = { name: "seppia", token: s_token}
+const polpo = { name: "polpo", token: p_token, showGui: false }
+const seppia = { name: "seppia", token: s_token, showGui: false }
 
 function spawnProcess(me : {
     name: string;
